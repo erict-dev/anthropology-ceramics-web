11c43a1071d8c57990a44af1020576cdc8be9ec8
Eric <eric@macbook-pro-2.lan>
2024-10-18 19:11:47 -0700

    fix jpg capitalize link

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -36,7 +36,7 @@ export default function About() {
         <div className="relative h-64 mt-4 mb-4">
           <Image
             alt=""
-            src="/home-studio-2-compressed.jpg"
+            src="/home-studio-2-compressed.JPG"
             layout="fill"
             objectFit="cover"
             className="absolute inset-0 rounded-lg"
@@ -46,7 +46,7 @@ export default function About() {
         <div className="relative h-64 mb-4">
           <Image
             alt=""
-            src="/home-studio-1-compressed.jpg"
+            src="/home-studio-1-compressed.JPG"
             layout="fill"
             objectFit="cover"
             className="absolute inset-0 rounded-lg"
@@ -55,7 +55,7 @@ export default function About() {
         <div className="relative h-64 mb-4">
           <Image
             alt=""
-            src="/home-studio-3-compressed.jpg"
+            src="/home-studio-3-compressed.JPG"
             layout="fill"
             objectFit="cover"
             className="absolute inset-0 rounded-lg"
@@ -64,7 +64,7 @@ export default function About() {
         <div className="relative h-64 mb-4">
           <Image
             alt=""
-            src="/home-studio-4-compressed.jpg"
+            src="/home-studio-4-compressed.JPG"
             layout="fill"
             objectFit="cover"
             className="absolute inset-0 rounded-lg"
