91591270833425f1a78b02cbd13d6c4c609d38b7
Eric <4564093+erict-dev@users.noreply.github.com>
2025-07-22 14:08:50 -0700

    add footer to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-// pages/Home.jsx
 import React from 'react';
 import Image from 'next/image';
 
+const customOffset = "-87px"
+
 const Home = () => {
   return (
-    <div className="home-background absolute top-0">
-      <div className="absolute inset-0 flex flex-col items-center justify-center">
+    <div className={`flex flex-col min-h-screen mb-[${customOffset}]`}>
+      {/* Splash Screen */}
+      <div className={`home-background flex flex-col items-center justify-center h-screen relative top-[${customOffset}]`}>
         <div className="relative w-full max-w-[680px] aspect-[3/2]">
           <Image
             src="/ac-logo-character-compressed.png"
@@ -15,10 +17,12 @@ const Home = () => {
             priority
           />
         </div>
-        <h2 className="text-3xl md:text-5xl leander">olomana studios</h2>
+        <h2 className="text-3xl md:text-5xl leander mt-4">olomana studios</h2>
       </div>
+      {/* Content below splash */}
     </div>
   );
 };
 
 export default Home;
+
