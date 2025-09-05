03a00f3cf1d6bb0c344d23eb6ec8400e06449bcd
Eric <4564093+erict-dev@users.noreply.github.com>
2025-09-04 22:15:58 -0700

    add redirect from 6-week to 4-week course url

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,16 @@ const nextConfig = {
     ],
   },
 
+  async redirects() {
+    return [
+      {
+        source: '/classes/6-week-pottery-course-irvine',
+        destination: '/classes/4-week-pottery-course-irvine',
+        permanent: true, // use false if you want a temporary redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
